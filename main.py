63ef3a425bc25034cc67d97d2621c3fdeb79c207
Matthias Crauwels <matthias.crauwels@planetscale.com>
2024-01-09 15:39:52 +0100

    configuration, argument parsing, error handling and bugfixing

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import contextlib
 import logging
 import sys
 import signal
-import _thread
+import threading
+import argparse
+import multiprocessing
 
 import grpc
+import yaml
 
 import psdbconnect.v1alpha1_pb2
 import psdbconnect.v1alpha1_pb2_grpc
@@ -54,7 +57,8 @@ def create_client_channel(address: str, username: str, password: str, port: int 
     yield channel
 
 
-def send_rpc(channel: grpc.Channel, request: psdbconnect.v1alpha1_pb2.SyncRequest) -> psdbconnect.v1alpha1_pb2.TableCursor:
+def send_rpc(channel: grpc.Channel, request: psdbconnect.v1alpha1_pb2.SyncRequest) \
+        -> psdbconnect.v1alpha1_pb2.TableCursor:
     stub = psdbconnect.v1alpha1_pb2_grpc.ConnectStub(channel)
 
     try:
@@ -63,24 +67,32 @@ def send_rpc(channel: grpc.Channel, request: psdbconnect.v1alpha1_pb2.SyncReques
         logging.error("Received error: %s", rpc_error)
         return rpc_error
     else:
-        for response in responses:
-            try:
-                logging.debug("Received message: %s", response)
-                _thread.start_new_thread(process_inserts, (response, ))
-                _thread.start_new_thread(process_updates, (response, ))
-                _thread.start_new_thread(process_deletes, (response, ))
+        try:
+            for response in responses:
+                try:
+                    logging.debug("Received message: %s", response)
+                    threading.Thread(target=process_inserts, args=(response, )).start()
+                    threading.Thread(target=process_updates, args=(response, )).start()
+                    threading.Thread(target=process_deletes, args=(response, )).start()
 
-            except grpc.RpcError as rpc_error:
-                logging.error("Received message: %s", rpc_error)
-                return response.cursor
+                except grpc.RpcError as rpc_error:
+                    logging.error("Received message: %s", rpc_error)
+                    return response.cursor
 
-        return response.cursor
+            return response.cursor
+        except Exception as e:
+            status = e.code()
+            details = e.details()
+            debug = e.debug_error_string()
+            logging.error(f"status: {status}, details: {details}")
+            logging.debug(debug)
+            return LAST_CURSOR
 
 
 def process_cursor(cursor: psdbconnect.v1alpha1_pb2.TableCursor):
     global LAST_CURSOR
     LAST_CURSOR = cursor
-    print(f"cursor: shard={cursor.shard}, keyspace={cursor.keyspace}, position={cursor.position}, lastKnownPK={cursor.last_known_pk}")
+    print(f"cursor: shard={cursor.shard}, keyspace={cursor.keyspace}, position={cursor.position}")
 
 
 def process_inserts(response: psdbconnect.v1alpha1_pb2.SyncResponse):
@@ -110,6 +122,8 @@ def print_row(result: vitess.query_pb2.QueryResult):
     output = []
     i = 0
     for field in result.fields:
+        if i == 0:
+            output.append(f"`{field.database}`.`{field.table}` ")
         for row in result.rows:
             output.append(f"{field.name} = {get_row_value(row, i)}")
             i += 1
@@ -121,50 +135,142 @@ def get_row_value(row: vitess.query_pb2.Row, field_num: int):
         return row.values[0:row.lengths[0]]
     else:
         start = 0
-        end = 0
         for i in range(0, field_num):
             start += row.lengths[i]
-            end += start + row.lengths[i+1]
+        end = start + row.lengths[field_num]
         return row.values[start:end]
 
 
-def run(username: str, password: str, keyspace: str, shard: str = "", last_position: str = ""):
+def run(connection, username: str, password: str, keyspace: str, table_name: str, columns=[], settings=[],
+        shard: str = "", last_position: str = ""):
     cursor = psdbconnect.v1alpha1_pb2.TableCursor(
         keyspace=keyspace,
         shard=shard,
         position=last_position
     )
+
     request = psdbconnect.v1alpha1_pb2.SyncRequest(
-        table_name="sbtest1",
-        columns=["id", "k", "c"],
+        table_name=table_name,
+        columns=columns,
         tablet_type=psdbconnect.v1alpha1_pb2.TabletType.replica,
         cursor=cursor,
-        include_inserts=True,
-        include_updates=True,
-        include_deletes=True,
+        include_inserts=settings['include_inserts'],
+        include_updates=settings['include_updates'],
+        include_deletes=settings['include_deletes'],
     )
 
-    with create_client_channel("aws.connect.psdb.cloud", username, password) as channel:
+    with create_client_channel(connection['hostname'], username, password, connection['port']) as channel:
         last_cursor = send_rpc(channel, request)
         process_cursor(last_cursor)
 
 
 def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!, stopping. Here is your final cursor:')
-    process_cursor(LAST_CURSOR)
+    print('You pressed Ctrl+C!, stopping.')
+    for process in processes:
+        process.terminate()
     sys.exit(0)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     signal.signal(signal.SIGINT, signal_handler)
+    parser = argparse.ArgumentParser(
+        prog="PythonPsdbConnect",
+        description="Example program for connecting to a PlanetScale Database using PlanetScale Connect.",
+        epilog="This is just an example, no guarantees are provided",
+    )
 
-    if len(sys.argv) < 4:
-        print(f"Usage: {sys.argv[0]} <username> <password> <keyspace> [shard] [position]")
-        exit(-1)
+    parser.add_argument("-f", "--config", default="config.yml", dest="config")
+    parser.add_argument("-l", "--log-level",
+                        default="info",
+                        choices=["info", "warning", "error", "debug"],
+                        dest="loglevel"
+                        )
+    parser.add_argument("shard", nargs="?")
+    parser.add_argument("position", nargs="?")
+    args = parser.parse_args()
+    # print(args)
 
-    if len(sys.argv) == 4:
-        run(sys.argv[1], sys.argv[2], sys.argv[3])
-    else:
-        run(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+    if args.loglevel == "info":
+        logging.basicConfig(level=logging.INFO)
+    elif args.loglevel == "warning":
+        logging.basicConfig(level=logging.WARNING)
+    elif args.loglevel == "error":
+        logging.basicConfig(level=logging.ERROR)
+    elif args.loglevel == "debug":
+        logging.basicConfig(level=logging.DEBUG)
+
+    with open(args.config, 'r') as file:
+        config = yaml.safe_load(file)
+        print(config)
+
+        if not config['credentials']:
+            print("Error: no credentials specified")
+            exit(-1)
+
+        if not config['tables']:
+            print("Error: no tables specified")
+            exit(-1)
+
+        if not config['credentials']['username'] \
+                or not config['credentials']['username']:
+            print("Error: username or password not specified")
+            exit(-1)
+
+        if not config['connection']:
+            config['connection'] = {
+                'hostname': 'aws.connect.psdb.cloud',
+                'port': 443,
+            }
+
+        if not config['connection']['hostname']:
+            config['connection']['hostname'] = 'aws.connect.psdb.cloud'
+
+        if not config['connection']['port']:
+            config['connection']['port'] = 443
+
+        if not config['connection']['keyspace']:
+            print("Error: keyspace not specified")
+            exit(-1)
+
+        if not config['settings']:
+            config['settings'] = {
+                'include_inserts': True,
+                'include_updates': True,
+                'include_deletes': True,
+            }
+
+        if not config['settings']['include_inserts']:
+            config['settings']['include_inserts'] = True
+
+        if not config['settings']['include_updates']:
+            config['settings']['include_updates'] = True
+
+        if not config['settings']['include_deletes']:
+            config['settings']['include_deletes'] = True
+
+        if not config['tables']:
+            print("Error: no tables specified")
+            exit(-1)
+
+        processes = list()
+        for table in config['tables']:
+            logging.info(f"Starting for table {table['name']}")
+            proc = multiprocessing.Process(target=run, args=(
+                config['connection'],
+                config['credentials']['username'],
+                config['credentials']['password'],
+                config['connection']['keyspace'],
+                table['name'],
+                table['columns'] if hasattr(table, 'column') else [],
+                config['settings'],
+                args.shard,
+                args.position,
+            ))
+            proc.start()
+            processes.append(proc)
+
+        for proc in processes:
+            proc.join()
+
+        logging.info("All done!")
 
